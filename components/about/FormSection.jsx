62502614f4533d7733af8b5924be34045bdeff5a
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-11-24 19:45:56 +0300

    24.11.2023

--- components/about/FormSection.jsx
+++ components/about/FormSection.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Input from "../Input";
 
-const FormSection = () => {
+const FormSection = ({
+  labelText,
+  inputName,
+  inputType,
+  inputPlaceholder,
+  inputValue,
+  inputOnChange,
+}) => {
   return (
-    <div>FormSection</div>
-  )
-}
+    <div className="grid gap-2">
+      <label htmlFor={inputName} className="font-medium cursor-pointer">
+        {labelText}
+      </label>
+      <Input
+        type={inputType}
+        id={inputName}
+        placeholder={inputPlaceholder}
+        value={inputValue}
+        onChange={inputOnChange}
+      />
+    </div>
+  );
+};
 
 export default FormSection

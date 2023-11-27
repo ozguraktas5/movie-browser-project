424d8e640ae4d66d4038c16dfae70747e02efad7
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-11-27 15:00:52 +0300

    27.11.2023

--- components/navbar/LinkButton.jsx
+++ components/navbar/LinkButton.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Link from "next/link";
 
-const LinkButton = () => {
+const LinkButton = ({ href, children, active}) => {
+
   return (
-    <div>LinkButton</div>
-  )
-}
+    <Link
+      href={href}
+      className={`p-2 text-center duration-300 grow rounded-xl font-medium ${
+        active
+          ? "text-neutral-50 bg-indigo-700/80"
+          : "hover:bg-indigo-700/80 hover:text-neutral-50"
+      }`}
+    >
+      {children}
+    </Link>
+  );
+};
 
 export default LinkButton
--- components/navbar/MobileBar.jsx
+++ components/navbar/MobileBar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import LinkButton from "./LinkButton";
 
-const MobileBar = () => {
+const MobileBar = ({ links, pathname }) => {
   return (
-    <div>MobileBar</div>
-  )
-}
+    <div className="flex items-center justify-between p-2 gap-2 md:hidden">
+      {links
+        .slice()
+        .reverse()
+        .map((link) => {
+          const isActive = pathname === link.href;
+          return (
+            <LinkButton key={link.href} href={link.href} active={isActive}>
+              {link.label}
+            </LinkButton>
+          );
+        })}
+    </div>
+  );
+};
 
-export default MobileBar
+export default MobileBar;
